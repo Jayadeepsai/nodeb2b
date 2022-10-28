96b667a67088a3ec96d39579249f5613e5f395e4
Jayadeepsai <jayadeepsaim@gmail.com>
2022-10-28 15:34:24 +0530

    first commit

---
+++ api/routes/orders.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const router = express.Router(); // a subpackage of express - reaching diff routes and diff endpoints , enable it by express.Funtion
+
+const mongoose = require('mongoose') ;
+const Order = require('../models/order');
+const Product = require('../models/order');
+
+
+router.get('/', (req, res, next) =>{
+   Order.find()
+   .select('product quantity _id')
+   .exec()
+   .then(result => {
+        res.status(200).json({
+            count: result.length,
+            orders: result.map(result => {
+                return {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000' + result._id
+                    }
+                }
+            })
+        })
+   })
+   .catch(err => {
+        res.status(404).json(err)
+   });
+}); 
+
+router.post('/', (req, res, next) =>{
+    Product.findById(req.body.productId)
+           .then(product=> {
+            if(!product) {
+               return res.status(404).json({
+                    message: 'Product not found'
+                });
+            }
+            const order = new Order({
+                _id: mongoose.Types.ObjectId(),
+                product: req.body.productId,          
+                quantity: req.body.quantity
+        
+            });
+             return order
+            .save()
+            
+           })
+
+           .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Order stored',
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000' + result._id
+                }
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(404).json({Error: err})
+        })
+    }); 
+
+router.get('/:orderId' ,(req, res, next) =>{
+    Order.findById(req.params.orderId)
+    .exec()
+    .then(order => {
+        if(!order) {
+            return res.status(404).json({
+                message: 'Order not found'
+            })
+        }
+        res.status(200).json({
+            order: order,
+            request: {
+                type: 'GET',
+                url: 'http://localhost:3000/orders'
+            }
+        })
+    })
+    .catch(err => {
+        res.status(404).json({error: err})
+    });
+});
+
+router.delete('/:orderId' , (req, res, next) =>{
+    const id = req.params.orderId;
+    Order.remove({_id:id})
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            messsage: 'Order deleted' ,
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3000/orders',
+                body: {productId: "ID" , quantity: "Number"}
+            } 
+        })
+    .catch(err => {
+        res.status(500).json({error: err})
+    })
+})
+});
+
+module.exports = router
+
---
+++ api/routes/products.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router(); 
+const mongoose = require('mongoose'); // a subpackage of express - reaching diff routes and diff endpoints , enable it by express.Funtion
+ 
+const Product = require('../models/product');
+
+
+router.get('/', (req, res, next) =>{
+   Product.find()
+   .exec()
+   .then(result => {
+    console.log(result);
+    res.status(200).json(result);
+   })
+   .catch(err => {
+    console.log(err);
+    res.status(500).json({Error: err});
+   })
+});
+
+
+router.post('/product', (req, res, next) =>{
+    const product =  new Product ({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,          
+        price: req.body.price
+    });
+      product.save().then(result => {
+        console.log(result);
+        res.status(201).json({
+            message:'Handling POST requests to /products',
+            createdProduct: product 
+      })
+    })
+      .catch(err => {console.log(err);
+      res.status(500).json({
+        error:err
+      });
+    });
+  
+});
+
+
+router.put('/', (req, res, next) =>{
+    res.status(200).json({
+        message:'Handling PUT requests to /products'
+    });
+});
+
+router.get('/:productId' ,(req, res, next) =>{
+    const id = req.params.productId;
+     Product.findById(id)
+     .exec()
+     .then(result => {
+        console.log(result);
+        res.status(201).json(result);
+     })
+     .catch(err => {
+        console.log(err);
+        res.status(500).json({Error: err});
+     })
+    });
+
+router.patch('/:productID' ,(req, res, next) =>{
+    res.status(200).json({
+        message: 'Updated product'
+    });
+});
+
+router.delete('/:productID' , (req, res, next) =>{
+   const id = req.params.productID;
+   Product.remove({_id: id}) 
+   .exec()
+   .then(result => {
+    console.log(result);
+    res.status(200).json(result)
+   }) 
+   .catch(err => {
+    console.log(err);
+    res.status(500).json({Error: err})
+   })
+});
+
+module.exports = router     
